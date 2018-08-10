e56d169f7bac36a82150f9c1f88fce3d546200ac
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-10 11:22:38 -0700

    methodsExercises
    exercises 1-3

---
+++ methodsExercise/Methods.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsExercise
+{
+    class Methods
+    {
+        public static int DivideNums(int num1)
+        {
+            int result = num1 / 2;
+            return result;
+        }
+
+        public static int MultiplyNums(int num1)
+        {
+            int result2 = num1 * 2;
+            return result2;
+        }
+
+        public static int AddNums(int num1)
+        {
+            int result3 = num1 + 2;
+            return result3;        
+        }
+
+
+
+
+
+    }
+}
--- methodsExercise/Program.cs
+++ methodsExercise/Program.cs
@@ -8,8 +8,26 @@ namespace methodsExercise
 {
     class Program
     {
+
+        //Methods exercises 1-3
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a whole number and I will do a few operations to it and return the results..");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            int answer1 = Methods.DivideNums(num1);
+            Console.WriteLine("Your number " + num1 + " divided by 2 = " + answer1);
+
+            int answer2 = Methods.MultiplyNums(num1);
+            Console.WriteLine("Your number " + num1 + " multiplied by 2 = " + answer2);
+
+            int answer3 = Methods.AddNums(num1);
+            Console.WriteLine("Your number " + num1 + " plus 2 " + answer3);
+
+            Console.ReadLine();
+
+
         }
     }
 }
